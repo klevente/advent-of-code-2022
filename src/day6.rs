6a284a91fe5805ec00f94b36c981dd978376ca53
Levente Krizsan <lewalevi@gmail.com>
2022-12-06 10:51:05 +0100

    refactor(day6): cleanup

--- src/day6.rs
+++ src/day6.rs
@@ -1,7 +1,7 @@
 use advent_of_code_2022::read_file_to_string;
 use itertools::Itertools;
 
-fn find_position_of_first_n_unique_chars(n: usize, s: &str) -> Option<usize> {
+fn find_position_of_first_n_unique_chars(s: &str, n: usize) -> Option<usize> {
     let v = s.chars().collect::<Vec<_>>();
     let result = v
         .windows(n)
@@ -13,10 +13,10 @@ fn main() {
     let input = read_file_to_string("input/day6.txt");
 
     let position_of_first_start_of_packet_marker =
-        find_position_of_first_n_unique_chars(4, &input).unwrap();
+        find_position_of_first_n_unique_chars(&input, 4).unwrap();
     println!("The position of the first start-of-packet marker is {position_of_first_start_of_packet_marker}");
 
     let position_of_first_start_of_message_marker =
-        find_position_of_first_n_unique_chars(14, &input).unwrap();
+        find_position_of_first_n_unique_chars(&input, 14).unwrap();
     println!("The position of the first start-of-message marker is {position_of_first_start_of_message_marker}");
 }
