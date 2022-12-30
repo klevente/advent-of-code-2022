5996fecedf5aa09143a258ae4585c8f04b3a958e
Levente Krizsán <lewalevi@gmail.com>
2022-12-30 18:53:19 +0100

    feat(day20): solve first assignment

--- src/day20.rs
+++ src/day20.rs
@@ -1,22 +1,117 @@
+use advent_of_code_2022::read_file_lines_as;
+use itertools::Itertools;
+use std::collections::HashMap;
+
+fn print_array(indices: &[(i64, usize)]) {
+    let out = indices
+        .iter()
+        .sorted_by(|(_, i), (_, j)| i.cmp(j))
+        .map(|(n, _)| *n)
+        .join(", ");
+    println!("[{out}]");
+}
+
+fn check_indices_unique_and_in_bounds(indices: &[(i64, usize)]) {
+    let iter = indices.iter().map(|(_, i)| *i);
+    let unique = iter.clone().all_unique();
+
+    if !unique {
+        let freq = indices
+            .iter()
+            .fold(HashMap::new(), |mut acc, (_, i)| {
+                *acc.entry(*i).or_insert(0) += 1;
+                acc
+            })
+            .into_iter()
+            .filter(|(_, f)| f > &1)
+            .collect::<HashMap<_, _>>();
+        dbg!(freq);
+        panic!("The indices were not unique");
+    }
+
+    let bounds = 0..indices.len();
+    let elements_out_of_bounds = iter
+        .clone()
+        .filter(|i| !bounds.contains(&i))
+        .collect::<Vec<_>>();
+
+    if !elements_out_of_bounds.is_empty() {
+        dbg!(elements_out_of_bounds);
+        panic!("The indices were not in bounds");
+    }
+}
+
+const KEY: i64 = 811589153;
+
 fn main() {
-    let mut arr = vec![1, 2, -3, 3, -2, 0, 4];
+    let arr = read_file_lines_as("input/day20.txt", |l| l.parse::<i64>().unwrap());
 
     let mut indices = arr
         .iter()
         .copied()
         .enumerate()
-        .map(|(i, n)| (n, i))
+        .map(|(i, n)| (n * 1, i))
         .collect::<Vec<_>>();
 
-    dbg!(&indices);
+    print_array(&indices);
 
+    // dbg!(&indices);
+
+    let n = indices.len() as i64;
     for i in 0..indices.len() {
-        let value = arr.remove(i) as i32;
-        let new_idx = value + i as i32;
-        arr.insert(new_idx as usize, value);
-        indices[i].1 = new_idx as usize;
-        for j in i..(new_idx as usize) {
-            indices[j].1 -= value as usize;
+        if i % 100 == 0 {
+            dbg!(i);
         }
+        let (value, old_idx) = indices[i];
+        let new_idx = if value > 0 {
+            let temp = value + old_idx as i64;
+            if temp >= n {
+                (temp + 1).rem_euclid(n)
+            } else {
+                temp
+            }
+        } else if value < 0 {
+            let temp = value + old_idx as i64;
+            if temp < 1 {
+                (temp - 1).rem_euclid(n)
+            } else {
+                temp
+            }
+        } else {
+            continue;
+        };
+        let new_idx = new_idx as usize;
+        indices[i].1 = new_idx;
+        // dbg!(new_idx);
+        let should_push_left = new_idx > old_idx;
+        let range_to_update = (old_idx.min(new_idx))..=(old_idx.max(new_idx));
+
+        for j in 0..indices.len() {
+            if i != j && range_to_update.contains(&indices[j].1) {
+                if should_push_left {
+                    indices[j].1 -= 1;
+                } else {
+                    indices[j].1 += 1;
+                }
+            }
+        }
+
+        check_indices_unique_and_in_bounds(&indices);
+        print_array(&indices);
+        // dbg!(&indices);
     }
+
+    print_array(&indices);
+    let position_of_0 = indices.iter().find(|(n, _)| n == &0).unwrap().1;
+    dbg!(position_of_0);
+    let position_of_x = (position_of_0 + 1000) % n as usize;
+    let position_of_y = (position_of_0 + 2000) % n as usize;
+    let position_of_z = (position_of_0 + 3000) % n as usize;
+
+    let x = indices.iter().find(|(_, i)| i == &position_of_x).unwrap().0;
+    let y = indices.iter().find(|(_, i)| i == &position_of_y).unwrap().0;
+    let z = indices.iter().find(|(_, i)| i == &position_of_z).unwrap().0;
+
+    dbg!(x, y, z);
+    dbg!(x + y + z);
 }
